7b57832a3998d568f0bab82daa5d1eaffe2e6679
Sky Bristol <skybristol@gmail.com>
2021-09-18 08:20:48 -0600

    Updated tests

--- tests/test_wd.py
+++ tests/test_wd.py
@@ -1,12 +1,11 @@
-import pytest
-from pymdt import wd
 import random
+import pymdt.wd
 
 
 def test_build_user_agent():
-    assert isinstance(wd.build_user_agent(), str)
+    assert isinstance(pymdt.wd.build_user_agent(), str)
 
 def test_get_wd_by_property():
-    property_key_value = random.choice(list(wd.property_mapping.keys()))
+    property_key_value = random.choice(list(pymdt.wd.property_mapping.keys()))
 
-    assert isinstance(wd.get_wd_by_property(property_key_value), list)
+    assert isinstance(pymdt.wd.get_wd_by_property(property_key_value), list)
